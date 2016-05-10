446f0778b4d765a6e79c88da241746d91e2f1cc7
Chris Taylor <christ@malfusion.org.uk>
2016-05-10 10:41:57 +0100

    Modified to work with latest modules

--- cvebuilder.py
+++ cvebuilder.py
@@ -27,8 +27,8 @@ from stix.extensions.marking.tlp import TLPMarkingStructure
 from stix.ttp import TTP, Behavior
 from stix.ttp.behavior import AttackPattern
 
-import common.ingest
-import common.taxii
+import common.ingest as ingest
+import common.taxii as taxii
 
 PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 
@@ -104,15 +104,15 @@ def _vulnbuild(data):
 def _postconstruct(xml, title):
     if CONFIG['ingest'][0]['active'] == True:
         try:
-            _inbox_package(CONFIG['ingest'][0]['endpoint'] +
-                           CONFIG['ingest'][0]['user'], xml)
+            ingest._inbox_package(CONFIG['ingest'][0]['endpoint'] +
+                                  CONFIG['ingest'][0]['user'], xml)
             print("[+] Successfully ingested " + title)
         except ValueError:
             print("[+] Failed ingestion for " + title)
     elif CONFIG['taxii'][0]['active'] == True:
         try:
-            _taxii(xml, CONFIG['taxii'][0]['host'], CONFIG['taxii'][0]['ssl'], CONFIG['taxii'][0]['discovery_path'], CONFIG['taxii'][
-                   0]['binding'], CONFIG['taxii'][0]['username'], CONFIG['taxii'][0]['password'], CONFIG['taxii'][0]['inbox_path'])
+            taxii._taxii(xml, CONFIG['taxii'][0]['host'], CONFIG['taxii'][0]['ssl'], CONFIG['taxii'][0]['discovery_path'], CONFIG['taxii'][
+                0]['binding'], CONFIG['taxii'][0]['username'], CONFIG['taxii'][0]['password'], CONFIG['taxii'][0]['inbox_path'])
             print("[+] Successfully inboxed " + title)
         except requests.exceptions.ConnectionError:
             print("[+] Failed inbox for " + title)
